569b244560e64295f1bc2d4f3c14864a3ec0b02c
Matt <200165243@student.georgianc.on.ca>
2025-11-28 19:27:14 -0500

    updated

--- lab-e/js/gallery.js
+++ lab-e/js/gallery.js
@@ -1,29 +1,51 @@
-// IMPORTANT: Before you start, be sure to customize all href="" values within the server include PHP files so your site navigation works
+console.log("gallery.js loaded");
 
-// STEP 1: Begin by adding a SCRIPT element to 'red/gallery.php' that refers to this EXTERNAL JavaScript File
+// Run this AFTER the HTML is parsed
+document.addEventListener("DOMContentLoaded", function () {
 
-// STEP 2a: Grab the UL for #blockGallery and assign it to the variable 'blockGallery' - but use document.getElementById this time
+  // STEP 2a: Grab the UL with id="blockGallery"
+  var blockGallery = document.getElementById("blockGallery");
+  if (!blockGallery) {
+    console.log("blockGallery UL not found");
+    return;
+  }
 
-// STEP 2b: Create an array called 'blockLinks' to contain all the A elements inside the above variable, 'blockGallery' - but use querySelectorAll instead of getElementsByTagName
+  // STEP 2b: Get all <a> elements inside the UL using querySelectorAll
+  var blockLinks = blockGallery.querySelectorAll("a");
 
-// STEP 2c: Make a loop that iterates through each item in the 'blockLinks' array that you just created above
+  // STEP 2c: Loop through each link
+  for (var i = 0; i < blockLinks.length; i++) {
 
-// STEP 3a: For each one of the A elements inside the array 'blockLinks', attach an onclick attribute that calls an anonymous function
+    // STEP 3a: Add onclick event with anonymous function
+    blockLinks[i].addEventListener("click", function (event) {
 
-// STEP 4a: For the link that was clicked, capture the value of the href attribute as a variable called 'blockHref'
+      // STEP 3c: Prevent default link navigation
+      event.preventDefault();
 
-// STEP 4b: Grab the IMG element just below the UL containing the links to each of the block images with the appropriate ID (look at the HTML in 'red/gallery.php') and assign it to a variable of the same name - use document.getElementById below
+      // STEP 4a: Get the href value of the clicked link
+      var blockHref = this.getAttribute("href");
 
-// STEP 4c: Change the src attribute of the above IMG element (use the variable name) using the setAttribute method, using the above 'blockHref' variable
+      // STEP 4b: Grab the main image by ID
+      var blockImage = document.getElementById("blockImage");
 
-// STEP 5a: Obtain the title attribute of the link clicked (referred to as 'this') and set it as the value for a new variable called 'blockCaption'
+      // STEP 4c: Update the image src
+      if (blockImage) {
+        blockImage.setAttribute("src", blockHref);
+      }
 
-// STEP 5b: Create another variable called 'blockCaptionParagraph' that represents the P element with the id 'blockCaption' - use either getElementById or querySelector
+      // STEP 5a: Get the title attribute of the clicked link
+      var blockCaption = this.getAttribute("title");
 
-// STEP 5c: Set the text of that paragraph to be equal to the value of the title attribute for the link clicked
+      // STEP 5b: Grab the P element for block caption
+      var blockCaptionParagraph = document.getElementById("blockCaption");
 
-// STEP 3c: Cancel the default behaviour of each hyperlink (which is to follow the href)
+      // STEP 5c: Update the caption text
+      if (blockCaptionParagraph) {
+        blockCaptionParagraph.textContent = blockCaption;
+      }
 
-// STEP 3b: End the 'onclick' event anonymous function
-
-// STEP 2d: End the loop through the 'blockLinks' array
+      // Extra safeguard to stop navigation
+      return false;
+    });
+  }
+});
